821f518cf3bb2799454f0a4fe45d0ddd08099917
dynamonda <v7gj9kk@gmail.com>
2017-06-03 22:49:18 +0900

    Check getSuit function in Card class

--- source/main.cpp
+++ source/main.cpp
@@ -2,11 +2,22 @@
 #include <stdlib.h>
 #include <iostream>
 #include <deck.h>
+#include <suit.h>
+#include <card.h>
 
 int main(){
 	std::cout << "Hello world" << std::endl;
 	Deck *deck = new Deck();
-	std::cout << deck->getCardNumber() << std::endl;
+	//std::cout << deck->getCardNumber() << std::endl;
+
+	Card *joker = new Card(0);
+	Card *card = new Card(1);
+
+	std::cout << joker->getSuit() << std::endl;
+	std::cout << card->getSuit() << std::endl;
+	
+	delete card;
+	delete joker;
 	delete deck;
 	return 0;
 }
