a42dd1a52cf5da41cac32ae37d84cc444a2d76dc
dynamonda <v7gj9kk@gmail.com>
2017-07-12 18:03:19 +0900

    Add hand.h

---
+++ include/hand.h
@@ -0,0 +1,19 @@
+// hand.h
+
+#ifndef _HAND_
+#define _HAND_
+
+enum class Hand : int{
+    ROYAL_FLUSH     = 0,
+    STRAIGHT_FLUSH  = 1,
+    FOUR_OF_A_KIND  = 2,
+    FULL_HOUSE      = 3,
+    FLUSH           = 4,
+    STRAIGHT        = 5,
+    THREE_OF_A_KIND = 6,
+    TWO_PAIR        = 7,
+    ONE_PAIR        = 8,
+    NO_PAIR         = 9,
+};
+
+#endif
