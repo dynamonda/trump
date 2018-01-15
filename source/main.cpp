b79c38557848fae2beaaf78d5e7e23343e31531c
dynamonda <v7gj9kk@gmail.com>
2018-01-15 14:43:03 +0900

    Fix main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,16 @@
-#include "card.h"
-#include "game.h"
+// main.cpp
 
 #include <iostream>
 #include <vector>
+
+#include "card.h"
+#include "game.h"
 
 int main(int argc, char *argv[]){
 	std::unique_ptr<Game> game = std::make_unique<Game>();
 	game->setup();
 	
-	while( !(game->isEnd()) ){
+	while(game->isEnd() == false){
 		game->update();
 	}
 
